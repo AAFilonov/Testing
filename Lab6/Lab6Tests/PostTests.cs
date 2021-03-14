6c3b55cbb49ee8e7d242f45db874b0f1ab5af06e
filon13p <filon13p@gmail.com>
2021-03-14 12:38:44 +0300

    создал 6

---
+++ Lab6/Lab6Tests/PostTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6.Tests
+{
+    [TestClass()]
+    public class PostTests
+    {
+        [TestMethod()]
+        public void CountRatingTCountRatingTest()
+        {
+            var post = new Post()
+            {
+                Author = "Auth",
+                Date = DateTime.Now,
+                Message = "123abc"
+            };
+
+            var rez = post.CountRating();
+            Assert.AreEqual(2 ,rez);
+
+
+            post.Message = "123";
+            rez = post.CountRating();
+            Assert.AreEqual(0.5, rez );
+
+
+            post.Message = "abc";
+            rez = post.CountRating();
+            Assert.AreEqual(2, rez);
+
+            post.Message = "";
+            rez = post.CountRating();
+            Assert.AreEqual(0, rez);
+
+
+            post.Message = "a a";
+            rez = post.CountRating();
+            Assert.AreEqual(2, rez);
+
+            post.Message = "aa aa";
+            rez = post.CountRating();
+            Assert.AreEqual(3, rez);
+        }
+
+        
+
+    }
+}
